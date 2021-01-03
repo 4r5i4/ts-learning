165dd35da3ebddb5a8825ac5e8b16110ede9883a
arsia ardalan <aardalan@rossvideo.com>
2021-01-03 05:28:06 -0500

    types, literal types

--- app.ts
+++ app.ts
@@ -2,14 +2,45 @@ console.log('first console log rendered here')
 
 function add(num1, num2){
     if(typeof num1 !== 'number' || typeof num2 !== 'number'){
-        throw new Error('Incorrect input')
+        // throw new Error('Incorrect input')
     }else{
         return num1 + num2
     }
 }
+
 
 const number1 = '2'
 const number2 = 2
 
 const result = add(number1, number2)
 console.log(result)
+
+
+const person= {
+    name: 'Arsia',
+    age: 3,
+    hobbies: ['soccer', 'cooking']
+}
+
+let favoriteActivitiesOrNumbers: (string|number)[];
+favoriteActivitiesOrNumbers = ['swimming', 18]
+
+console.log(person.name, ', and he is ', person.age, ' years old apparently')
+
+
+// you can have literal types as is the case for the 'identifier'
+function decideNumberOrText(value: string | number , identifier: 'as-text' | 'as-number'){
+    if(identifier === 'as-text'){
+        console.log(`${value.toString()} is a text`)
+    } else if (identifier === 'as-number') {
+        console.log(`${value} is a number`);
+    } else {
+        console.log('ERROR, identifier has to be either as-text or as-number!')
+    }
+
+}
+
+decideNumberOrText(12, 'as-text')
+decideNumberOrText('hello there, how are you?', 'as-text')
+decideNumberOrText(0.0834, 'as-number')
+decideNumberOrText((0.0834 + 'hello'), 'as-text')
