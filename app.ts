021fe6f8ffd661b0505a4651f86fbfbbacf738a0
arsia ardalan <aardalan@rossvideo.com>
2021-01-02 19:33:43 -0500

    adds typescript dep

---
+++ app.ts
@@ -0,0 +1,11 @@
+console.log('first console log rendered here')
+
+function add(num1, num2){
+    return num1 + num2
+}
+
+const number1 = 1
+const number2 = 2
+
+const result = add(number1, number2)
+console.log(result)
