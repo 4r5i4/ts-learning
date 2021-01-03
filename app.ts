e5ad4ea48f03d0399e80e263f63cb3f50e00983c
arsia ardalan <aardalan@rossvideo.com>
2021-01-02 19:42:59 -0500

    you can do typechecking with JS but throwing error is not necessary and it only is thrown during runtime, not during development

--- app.ts
+++ app.ts
@@ -1,10 +1,14 @@
 console.log('first console log rendered here')
 
 function add(num1, num2){
-    return num1 + num2
+    if(typeof num1 !== 'number' || typeof num2 !== 'number'){
+        throw new Error('Incorrect input')
+    }else{
+        return num1 + num2
+    }
 }
 
-const number1 = 1
+const number1 = '2'
 const number2 = 2
 
 const result = add(number1, number2)
