69da0af415cedbc709fc4703d994e5b05ba2c6fe
arsia ardalan <aardalan@rossvideo.com>
2021-01-03 08:29:38 -0500

    function types

--- app.ts
+++ app.ts
@@ -90,3 +90,52 @@ decideNumberOrText(12, 'as-text')
 decideNumberOrText('hello there, how are you?', 'as-text')
 decideNumberOrText(0.0834, 'as-number')
 decideNumberOrText((0.0834 + 'hello'), 'as-text')
+
+
+
+/////////////////////////////////////
+function multiplyTwoNumbers(number1: number, number2: number){
+    return number1 * number2;
+}
+
+let multiplicationResult: number;
+let multiplicationResultPointer: Function;
+
+// function returns the value therefore the type of this variable should be number
+multiplicationResult = multiplyTwoNumbers(23, 423)
+console.log('multiplicationPointer Result: ',multiplicationResult)
+
+multiplicationResultPointer = multiplyTwoNumbers; // this is a pointer to the function 
+console.log('multiplicationPointer Result: ', multiplicationResultPointer(23, 423))
+
+// multiplicationResultPointer = 'what the hell' // we are reassigning the variable, it is no longer pointing to a function
+// console.log('multiplicationPointer Result: ', multiplicationResultPointer(23, 423)) //ERROR: multiplicationResultPointer is not a function
+
+// now if we point the multiplicationPointer to another Function, we shouldn't have a problem right? 
+function displaySomeText() { // the return type of this function is 'void' since it does not return anything
+    console.log('some text')
+}
+
+console.log(displaySomeText())  // 'undefined' which is a valid value in JS. The function simply does not retrun anything
+displaySomeText()   // displays the text
+
+// now lets point the multiplicationPointer to displaySomeText, which is a valid function:
+multiplicationResultPointer = displaySomeText
+console.log('multiplicationPointer Result: ', multiplicationResultPointer(23, 423)) //undefined because displaySomeText() does not return anything
+
+// so it would be nice if we could be more specific about the function, how it looks and such, so we have function types. 
+// Function types are types that describe a function and return value of that function.
+// the syntax looks a bit like an arrow function but it is not:
+
+let someResult: (a: number, b: number) => number; // what this is saying, is that someResult accepts any function that returns the value of type number and takes no parameters
+// someResult = displaySomeText // Type '() => void' is not assignable to type '(a: number, b: number) => number'.  Type 'void' is not assignable to type 'number'.
+
+// As for callbacks, we can do the same:
+function addAndHandle(n1: number, n2: number, cb: (num: number)=> void){
+    const result = n1+n2;
+    cb(result)
+}
+
+addAndHandle(2, 343, (someResult)=>{
+    console.log('The result from the CallBack function is: ' , someResult)
+})
